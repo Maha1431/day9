c7b64068af3998a61130524b3105ffd3b6338f0e
maha <mmahaece072gmail.com>
2021-10-29 16:25:59 +0530

    Add project files.

---
+++ day9/uc4.cs
@@ -0,0 +1,35 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day9
+{
+    class uc4
+    {
+        public const int Is_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        static void Main(string[] args)
+        {
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            switch (empCheck)
+            {
+                case Is_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+            }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("EMP WAGE :" + empWage);
+        }
+    }
+}
+*/
